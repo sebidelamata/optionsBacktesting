0dea87f8f24d4d6d55814b63fc943b2eb80e8dc4
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2022-05-04 17:49:47 -0600

    created script to grab watchlist

---
+++ grab_watchlist.R
@@ -0,0 +1,41 @@
+# grab our scraper script
+source("scraper.R")
+
+# declare the tickers we want in our watchlist
+watchlist <- c(
+  "QQQ",
+  "SPY",
+  "IWM",
+  "SLYV",
+  "FXI",
+  "DIA",
+  "ARKK",
+  "FEZ",
+  "EEM",
+  "EWW",
+  "EWZ",
+  "XLB",
+  "XLV",
+  "XLU",
+  "XLF",
+  "XLI",
+  "XOP",
+  "GLD",
+  "SLV",
+  "TLT",
+  "HYG"
+)
+
+# create an empty dataframe 
+watchlist_data <- data.frame()
+
+# for each ticker in the watchlist grab the option data
+# and union it to the watchlist_data df
+watchlist_data <- do.call(
+  rbind,
+  lapply(
+    watchlist,
+    grab_option_data
+    )
+  )
+
