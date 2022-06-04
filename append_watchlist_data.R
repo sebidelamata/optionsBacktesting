475bfcadec476774b74c85dc96aa44b9abc465de
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2022-06-04 13:21:44 -0600

    sql query create table

---
+++ append_watchlist_data.R
@@ -0,0 +1,29 @@
+library(RPostgreSQL)
+library(getPass)
+
+source("grab_watchlist.R")
+
+
+watchlist_data <- grab_watchlist()
+
+
+driver_name <- dbDriver(drvName = "PostgreSQL")
+
+db <- DBI::dbConnect(driver_name,
+                     dbname="sebi",
+                     host="localhost",
+                     port = 5432,
+                     user = "sebi",
+                     password = getPass("Enter Password:")
+)
+
+
+DBI::dbWriteTable(
+  db,
+  "watchlist_data",
+  watchlist_data,
+  append = TRUE
+)
+
+
+DBI::dbDisconnect(db)
