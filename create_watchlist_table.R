6d1b28ea569e90718daad18fbc213a9156694b60
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2022-06-04 13:01:44 -0600

    sql query create table

--- create_watchlist_table.R
+++ create_watchlist_table.R
@@ -11,50 +11,78 @@ db <- DBI::dbConnect(driver_name,
 		     password = getPass("Enter Password:")
 		     )
 
-
-create_table_query_one <- 'CREATE SCHEMA [IF NOT EXISTS] options;'
-
-create_table_query_two <- 'CREATE TABLE [IF NOT EXISTS] options.watchlist_data (
-  option VARCHAR ( 32 ) PRIMARY KEY,
-  bid NUMERIC ( 8 ) NOT NULL,
-  bid_size INT NOT NULL,
-  ask NUMERIC ( 8 ) NOT NULL,
-  ask_size INT NOT NULL,
-  iv NUMERIC ( 8 ) NOT NULL,
-  open_interest INT NOT NULL,
-  volume INT NOT NULL,
-  delta NUMERIC ( 8 ) NOT NULL,
-  gamma NUMERIC ( 8 ) NOT NULL,
-  theta NUMERIC ( 8 ) NOT NULL,
-  rho NUMERIC ( 8 ) NOT NULL,
-  vega NUMERIC ( 8 ) NOT NULL,
-  theo NUMERIC ( 8 ) NOT NULL,
-  change NUMERIC ( 8 ) NOT NULL,
-  open NUMERIC ( 8 ) NOT NULL,
-  high NUMERIC ( 8 ) NOT NULL,
-  low NUMERIC ( 8 ) NOT NULL,
-  tick VARCHAR ( 4 ) NOT NULL,
-  last_trade_price NUMERIC ( 8 ) NULL,
-  last_trade_time TIMESTAMP NULL,
-  percent_change NUMERIC ( 8 ) NULL,
-  prev_day_close NUMERIC ( 8 ) NULL,
-  underlying_close NUMERIC ( 8 ) NOT NULL,
-  strike_price NUMERIC ( 8 ) NOT NULL,
-  contract_type CHAR ( 1 ) NOT NULL,
-  underlying_ticker VARCHAR ( 4 ) NOT NULL,
-  expiration_date DATE NOT NULL,
-  scrape_date DATE NOT NULL
-  );
-  '
-
-DBI::dbSendQuery(
-  db,
-  create_table_query_one
+watchlist_data <- data.frame(
+  option = character(),
+  bid = numeric(),
+  bid_size = integer(),
+  ask = numeric(),
+  ask_size = integer(),
+  iv = numeric(),
+  open_interest = integer(),
+  volume = integer(),
+  delta = numeric(),
+  gamma = numeric(),
+  theta = numeric(),
+  rho = numeric(),
+  vega = numeric(),
+  theo = numeric(),
+  change = numeric(),
+  open = numeric(),
+  high = numeric(),
+  low = numeric(),
+  tick = character(),
+  last_trade_price = numeric(),
+  last_trade_time = POSIXct(),
+  percent_change = numeric(),
+  prev_day_close = numeric(),
+  underlying_close = numeric(),
+  strike_price = numeric(),
+  contract_type = character(),
+  underlying_ticker = character(),
+  expiration_date = POSIXct(),
+  scrape_date = POSIXct()
 )
 
-DBI::dbSendQuery(
+
+# create_table_query_one <- 'CREATE SCHEMA [IF NOT EXISTS] options;'
+# 
+# create_table_query_two <- 'CREATE TABLE [IF NOT EXISTS] options.watchlist_data (
+#   option VARCHAR ( 32 ) PRIMARY KEY,
+#   bid NUMERIC ( 8 ) NOT NULL,
+#   bid_size INT NOT NULL,
+#   ask NUMERIC ( 8 ) NOT NULL,
+#   ask_size INT NOT NULL,
+#   iv NUMERIC ( 8 ) NOT NULL,
+#   open_interest INT NOT NULL,
+#   volume INT NOT NULL,
+#   delta NUMERIC ( 8 ) NOT NULL,
+#   gamma NUMERIC ( 8 ) NOT NULL,
+#   theta NUMERIC ( 8 ) NOT NULL,
+#   rho NUMERIC ( 8 ) NOT NULL,
+#   vega NUMERIC ( 8 ) NOT NULL,
+#   theo NUMERIC ( 8 ) NOT NULL,
+#   change NUMERIC ( 8 ) NOT NULL,
+#   open NUMERIC ( 8 ) NOT NULL,
+#   high NUMERIC ( 8 ) NOT NULL,
+#   low NUMERIC ( 8 ) NOT NULL,
+#   tick VARCHAR ( 4 ) NOT NULL,
+#   last_trade_price NUMERIC ( 8 ) NULL,
+#   last_trade_time TIMESTAMP NULL,
+#   percent_change NUMERIC ( 8 ) NULL,
+#   prev_day_close NUMERIC ( 8 ) NULL,
+#   underlying_close NUMERIC ( 8 ) NOT NULL,
+#   strike_price NUMERIC ( 8 ) NOT NULL,
+#   contract_type CHAR ( 1 ) NOT NULL,
+#   underlying_ticker VARCHAR ( 4 ) NOT NULL,
+#   expiration_date DATE NOT NULL,
+#   scrape_date DATE NOT NULL
+#   );
+#   '
+
+DBI::dbWriteTable(
   db,
-  create_table_query_two
+  watchlist_data
 )
+
 
 DBI::dbDisconnect(db)
