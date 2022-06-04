a9f8a787edb01095eb5240d55be932b7d72857c2
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2022-06-04 14:45:23 -0600

    sql query create table

--- append_watchlist_data.R
+++ append_watchlist_data.R
@@ -18,13 +18,52 @@ db <- DBI::dbConnect(driver_name,
 )
 
 
-DBI::dbWriteTable(
-  db,
-  "watchlist_data",
-  watchlist_data,
-  row.names = FALSE,
-  append = TRUE
-)
+update <- dbSendQuery(
+  db, 
+  'insert into watchlist_data set "option = ?,
+  bid = ?,
+  bid_size = ?,
+  ask = ?,
+  ask_size = ?,
+  iv = ?,
+  open_interest = ?,
+  volume = ?,
+  delta = ?,
+  gamma = ?,
+  theta = ?,
+  rho = ?
+  vega = ?,
+  theo = ?,
+  change = ?,
+  open = ?,
+  high = ?,
+  low = ?,
+  tick = ?,
+  last_trade_price = ?,
+  last_trade_time = ?,
+  percent_change = ?,
+  prev_day_close = ?,
+  underlying_close = ?,
+  strike_price = ?,
+  contract_type = ?,
+  underlying_ticker = ?,
+  expiration_date = ?,
+  scrape_date = ?'
+  )
+
+
+dbBind(update, watchlist_data)  # send the updated data
+
+
+dbClearResult(update)
+
+# DBI::dbWriteTable(
+#   db,
+#   "watchlist_data",
+#   watchlist_data,
+#   row.names = FALSE,
+#   append = TRUE
+# )
 
 
 DBI::dbDisconnect(db)
