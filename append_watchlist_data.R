c68943f1d8c2c06d4f0172d72ffe8635b7f6e4ce
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2022-06-05 14:41:20 -0600

    error testing for database writes

--- append_watchlist_data.R
+++ append_watchlist_data.R
@@ -4,71 +4,44 @@ library(getPass)
 source("grab_watchlist.R")
 
 
-watchlist_data <- grab_watchlist()
+append_watchlist_data <- function(){
+  
+  # grab our watchlist data
+  watchlist_data <- grab_watchlist()
+  
+  # establish driver name
+  driver_name <- dbDriver(drvName = "PostgreSQL")
+  
+  # create database connection
+  db <- DBI::dbConnect(driver_name,
+                       dbname="sebi",
+                       host="localhost",
+                       port = 5432
+  )
+  
+  # append our scraped data to the table
+  DBI::dbWriteTable(
+    db,
+    name = "watchlist_data",
+    value = watchlist_data,
+    row.names = FALSE,
+    append = TRUE
+  )
+  
+  # close database connection
+  DBI::dbDisconnect(db)
+}
 
-
-driver_name <- dbDriver(drvName = "PostgreSQL")
-
-db <- DBI::dbConnect(driver_name,
-                     dbname="sebi",
-                     host="localhost",
-                     port = 5432
+# create try catch function
+error_test <- tryCatch(
+  
+  {
+  # run our main function
+  append_watchlist_data()
+  },
+  {
+    error = print("Error writing data to table")
+  }
 )
 
-# update <- paste0("INSERT INTO public.watchlist_data VALUES ('",watchlist_data$bid[1],"','",watchlist_data$bid_size[1],"','",watchlist_data$ask[1],"','",watchlist_data$ask_size[1],"','",watchlist_data$iv[1],"','",watchlist_data$volume[1],"','",watchlist_data$delta[1],"','",watchlist_data$gamma[1],"','",watchlist_data$theta[1],"','",watchlist_data$rho[1],"','",watchlist_data$vega[1],"','",watchlist_data$theo[1],"','",watchlist_data$change[1],"','",watchlist_data$open[1],"','",watchlist_data$high[1],"','",watchlist_data$low[1],"','",watchlist_data$tick[1],"','",watchlist_data$last_trade_price[1],"','",watchlist_data$last_trade_time[1],"','",watchlist_data$percent_change[1],"','",watchlist_data$prev_day_close[1],"','",watchlist_data$underlying_close[1],"','",watchlist_data$stike_price[1],"','",watchlist_data$contract_type[1],"','",watchlist_data$underlying_ticker[1],"','",watchlist_data$expiration_date[1],"','",watchlist_data$scrape_date[1],"');")
-# 
-# 
-# update <- dbSendQuery(
-#   db,
-#   update
-#   )
-# 
-# dbClearResult(update)
-
-# table_id <- DBI::Id(
-#   db,
-#   schema  = "public",
-#   table   = "watchlist_data"
-# )
-
-DBI::dbWriteTable(
-  db,
-  name = "watchlist_data",
-  value = watchlist_data,
-  # field.types = c(
-  #   option = "text",
-  #   bid = "double precision",
-  #   bid_size = "integer",
-  #   ask = "double precision",
-  #   ask_size = "integer",
-  #   iv = "double precision",
-  #   open_interest = "integer",
-  #   volume = "integer",
-  #   delta = "double precision",
-  #   gamma = "double precision",
-  #   theta = "double precision",
-  #   rho = "double precision",
-  #   vega = "double precision",
-  #   theo = "double precision",
-  #   change = "double precision",
-  #   open = "double precision",
-  #   high = "double precision",
-  #   low = "double precision",
-  #   tick = "text",
-  #   last_trade_price = "double precision",
-  #   last_trade_time = "double precision",
-  #   percent_change = "double precision",
-  #   prev_day_close = "double precision",
-  #   underlying_close = "double precision",
-  #   strike_price = "double precision",
-  #   contract_type = "text",
-  #   underlying_ticker = "text",
-  #   expiration_date = "double precision",
-  #   scrape_date = "double precision"
-  # ),
-  row.names = FALSE,
-  append = TRUE
-)
-
-
-DBI::dbDisconnect(db)
+error_test
