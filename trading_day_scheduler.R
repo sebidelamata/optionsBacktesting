99ddaa2bf68f1c69ae75a0c09d19c3e169151697
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2022-06-05 16:37:32 -0600

    absolute directories and cronjobs

--- trading_day_scheduler.R
+++ trading_day_scheduler.R
@@ -2,7 +2,7 @@ library(timeDate)
 
 source("/home/sebi/optionsBacktesting/append_watchlist_data.R")
 
-if(as.POSIXlt(Sys.Date())$wday %in% 1:5 & Sys.Date() !(%in% as.Date(timeDate::holidayNYSE()))){
+if(as.POSIXlt(Sys.Date())$wday %in% 1:5 & !(Sys.Date() %in% as.Date(timeDate::holidayNYSE()))){
   append_watchlist_data()
 } else {
   message("Market Closed Today")
