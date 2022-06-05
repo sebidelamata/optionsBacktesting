ad1b9336d28bd65111bcb4379514aef43b07873e
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2022-06-04 21:32:07 -0600

    sql query create table

--- append_watchlist_data.R
+++ append_watchlist_data.R
@@ -17,54 +17,54 @@ db <- DBI::dbConnect(driver_name,
                      password = getPass("Enter Password:")
 )
 
-update <- paste0("INSERT INTO public.watchlist_data VALUES ('",watchlist_data$bid[1],"','",watchlist_data$bid_size[1],"','",watchlist_data$ask[1],"','",watchlist_data$ask_size[1],"','",watchlist_data$iv[1],"','",watchlist_data$volume[1],"','",watchlist_data$delta[1],"','",watchlist_data$gamma[1],"','",watchlist_data$theta[1],"','",watchlist_data$rho[1],"','",watchlist_data$vega[1],"','",watchlist_data$theo[1],"','",watchlist_data$change[1],"','",watchlist_data$open[1],"','",watchlist_data$high[1],"','",watchlist_data$low[1],"','",watchlist_data$tick[1],"','",watchlist_data$last_trade_price[1],"','",watchlist_data$last_trade_time[1],"','",watchlist_data$percent_change[1],"','",watchlist_data$prev_day_close[1],"','",watchlist_data$underlying_close[1],"','",watchlist_data$stike_price[1],"','",watchlist_data$contract_type[1],"','",watchlist_data$underlying_ticker[1],"','",watchlist_data$expiration_date[1],"','",watchlist_data$scrape_date[1],"');")
-
-
-update <- dbSendQuery(
-  db,
-  update
-  )
+# update <- paste0("INSERT INTO public.watchlist_data VALUES ('",watchlist_data$bid[1],"','",watchlist_data$bid_size[1],"','",watchlist_data$ask[1],"','",watchlist_data$ask_size[1],"','",watchlist_data$iv[1],"','",watchlist_data$volume[1],"','",watchlist_data$delta[1],"','",watchlist_data$gamma[1],"','",watchlist_data$theta[1],"','",watchlist_data$rho[1],"','",watchlist_data$vega[1],"','",watchlist_data$theo[1],"','",watchlist_data$change[1],"','",watchlist_data$open[1],"','",watchlist_data$high[1],"','",watchlist_data$low[1],"','",watchlist_data$tick[1],"','",watchlist_data$last_trade_price[1],"','",watchlist_data$last_trade_time[1],"','",watchlist_data$percent_change[1],"','",watchlist_data$prev_day_close[1],"','",watchlist_data$underlying_close[1],"','",watchlist_data$stike_price[1],"','",watchlist_data$contract_type[1],"','",watchlist_data$underlying_ticker[1],"','",watchlist_data$expiration_date[1],"','",watchlist_data$scrape_date[1],"');")
+# 
+# 
+# update <- dbSendQuery(
+#   db,
+#   update
+#   )
 # 
 # dbClearResult(update)
 
-# DBI::dbWriteTable(
-#   db,
-#   name = c("public","watchlist_data"),
-#   value = watchlist_data[1,],
-#   field.types = c(
-#     option = "text",
-#     bid = "double precision",
-#     bid_size = "integer",
-#     ask = "double precision",
-#     ask_size = "integer",
-#     iv = "double precision",
-#     open_interest = "integer",
-#     volume = "integer",
-#     delta = "double precision",
-#     gamma = "double precision",
-#     theta = "double precision",
-#     rho = "double precision",
-#     vega = "double precision",
-#     theo = "double precision",
-#     change = "double precision",
-#     open = "double precision",
-#     high = "double precision",
-#     low = "double precision",
-#     tick = "text",
-#     last_trade_price = "double precision",
-#     last_trade_time = "double precision",
-#     percent_change = "double precision",
-#     prev_day_close = "double precision",
-#     underlying_close = "double precision",
-#     strike_price = "double precision",
-#     contract_type = "text",
-#     underlying_ticker = "text",
-#     expiration_date = "double precision",
-#     scrape_date = "double precision"
-#   ),
-#   row.names = FALSE,
-#   append = TRUE
-# )
+DBI::dbWriteTable(
+  db,
+  name = c("public","watchlist_data"),
+  value = watchlist_data[1,],
+  field.types = c(
+    option = "text",
+    bid = "double precision",
+    bid_size = "integer",
+    ask = "double precision",
+    ask_size = "integer",
+    iv = "double precision",
+    open_interest = "integer",
+    volume = "integer",
+    delta = "double precision",
+    gamma = "double precision",
+    theta = "double precision",
+    rho = "double precision",
+    vega = "double precision",
+    theo = "double precision",
+    change = "double precision",
+    open = "double precision",
+    high = "double precision",
+    low = "double precision",
+    tick = "text",
+    last_trade_price = "double precision",
+    last_trade_time = "double precision",
+    percent_change = "double precision",
+    prev_day_close = "double precision",
+    underlying_close = "double precision",
+    strike_price = "double precision",
+    contract_type = "text",
+    underlying_ticker = "text",
+    expiration_date = "double precision",
+    scrape_date = "double precision"
+  ),
+  row.names = FALSE,
+  append = TRUE
+)
 
 
 DBI::dbDisconnect(db)
