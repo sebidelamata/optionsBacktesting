8d0de33ca9a95bac31eab7cb904b2ce163d728be
Miguel Sebastian de la Mata <delamatas@appstate.edu>
2022-06-04 20:41:32 -0600

    sql query create table

--- append_watchlist_data.R
+++ append_watchlist_data.R
@@ -17,6 +17,8 @@ db <- DBI::dbConnect(driver_name,
                      password = getPass("Enter Password:")
 )
 
+# update <- paste0("INSERT INTO myschema.fruits VALUES ('",watchlist_data$bid,"','",watchlist_data$bid_size,"','",watchlist_data$ask,"','",watchlist_data$ask_size,"','",watchlist_data$iv,"','",watchlist_data$volume,"','",watchlist_data$delta,"','",watchlist_data$gamma,"','",watchlist_data$theta,"','",watchlist_data$rho,"','",watchlist_data$vega,"','",watchlist_data$theo,"','",watchlist_data$change,"','",watchlist_data$open,"','",watchlist_data$high,"','",watchlist_data$low,"','",watchlist_data$tick,"','",watchlist_data$last_trade_price,"','",watchlist_data$last_trade_time,"','",watchlist_data$percent_change,"','",watchlist_data$prev_day_close,"','",watchlist_data$underlying_close,"','",watchlist_data$stike_price,"','",watchlist_data$contract_type,"','",watchlist_data$bid,"','",watchlist_data$bid_size,"','",watchlist_data$ask,"','",watchlist_data$ask_size,"','",watchlist_data$iv,"','",watchlist_data$volume,"','",watchlist_data$delta,"','",watchlist_data$gamma,"','",watchlist_data$theta,"','",watchlist_data$rho,"','",watchlist_data$vega,"','",watchlist_data$theo,"','",watchlist_data$change,"','",watchlist_data$open,"','",watchlist_data$high,"','",watchlist_data$low,"','",watchlist_data$tick,"','",watchlist_data$last_trade_price,"','",watchlist_data$last_trade_time,"','",watchlist_data$percent_change,"','",watchlist_data$prev_day_close,"','",watchlist_data$underlying_close,"','",watchlist_data$stike_price,"');")
+
 # 
 # update <- dbSendQuery(
 #   db, 
@@ -61,6 +63,37 @@ DBI::dbWriteTable(
   db,
   name = c("public","watchlist_data"),
   value = watchlist_data[1,],
+  field.types = c(
+    option = character(),
+    bid = numeric(),
+    bid_size = integer(),
+    ask = numeric(),
+    ask_size = integer(),
+    iv = numeric(),
+    open_interest = integer(),
+    volume = integer(),
+    delta = numeric(),
+    gamma = numeric(),
+    theta = numeric(),
+    rho = numeric(),
+    vega = numeric(),
+    theo = numeric(),
+    change = numeric(),
+    open = numeric(),
+    high = numeric(),
+    low = numeric(),
+    tick = character(),
+    last_trade_price = numeric(),
+    last_trade_time = numeric(),
+    percent_change = numeric(),
+    prev_day_close = numeric(),
+    underlying_close = numeric(),
+    strike_price = numeric(),
+    contract_type = character(),
+    underlying_ticker = character(),
+    expiration_date = numeric(),
+    scrape_date = numeric()
+  ),
   row.names = FALSE,
   append = TRUE
 )
